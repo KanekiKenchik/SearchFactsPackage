64d9ead2aeac405d467aab481c8330e9f1f33ed1
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-26 20:28:15 +0300

    test

--- Sources/SwiftExamplePackage/SearchFactsViewController.swift
+++ Sources/SwiftExamplePackage/SearchFactsViewController.swift
@@ -10,9 +10,25 @@ import SnapKit
 
 public class SearchFactsViewController: UIViewController {
     
+    var lbl: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "test"
+        lbl.font = UIFont(name: "Avenir Medium", size: 30)
+        return lbl
+    }()
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+    }
+    
+    private func setup() {
         view.backgroundColor = .red
+        view.addSubview(lbl)
+        
+        lbl.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
     }
 }
