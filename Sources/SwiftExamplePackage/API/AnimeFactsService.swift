a7f5f14e84a4f89b216d1cf690d8902f01157448
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-27 19:57:57 +0300

    test

--- Sources/SwiftExamplePackage/API/AnimeFactsService.swift
+++ Sources/SwiftExamplePackage/API/AnimeFactsService.swift
@@ -11,20 +11,29 @@ import Alamofire
 class AnimeFactsService {
     
     func getFactsAbout(animeName: String, completion: @escaping (SearchFactsEntity) -> Void) {
-        if animeName != "", animeName.count > 5 {
-            AF.request("https://anime-facts-rest-api.herokuapp.com/api/v1/\(animeName)").validate().responseData(completionHandler: { responseData in
-                switch responseData.result {
-                case .success(let value):
-                    do {
-                        let animeFacts = try JSONDecoder().decode(SearchFactsEntity.self, from: value)
-                        completion(animeFacts)
-                    } catch {
-                        print("Error decoding jsonData: \(error.localizedDescription)")
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            })
+//        if animeName != "", animeName.count > 5 {
+//            AF.request("https://anime-facts-rest-api.herokuapp.com/api/v1/\(animeName)").validate().responseData(completionHandler: { responseData in
+//                switch responseData.result {
+//                case .success(let value):
+//                    do {
+//                        let animeFacts = try JSONDecoder().decode(SearchFactsEntity.self, from: value)
+//                        completion(animeFacts)
+//                    } catch {
+//                        print("Error decoding jsonData: \(error.localizedDescription)")
+//                    }
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            })
+//        }
+        let animeFacts = [AnimeFact]()
+        for i in 0..<10 {
+            let animeFact = AnimeFact(fact_id: i+1, fact: "Fact №\(i+1)")
+            animeFacts.append(animeFact)
         }
+        
+        let anime = SearchFactsEntity(img: "", total_facts: 10, data: animeFacts)
+        
+        completion(anime)
     }
 }
