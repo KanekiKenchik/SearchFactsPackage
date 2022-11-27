6697e30489c0632bae9e51ca04466bb68fcdcbc4
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-27 19:01:29 +0300

    test

--- Sources/SwiftExamplePackage/SearchFactsViewController.swift
+++ Sources/SwiftExamplePackage/SearchFactsViewController.swift
@@ -8,111 +8,29 @@
 import UIKit
 import SnapKit
 
-protocol SearchFactsViewProtocol: AnyObject {
-    func showAnimeFacts(animeFacts: SearchFactsEntity?)
-}
 
 public class SearchFactsViewController: UIViewController {
     
-    public let tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.register(SearchFactsTableViewCell.self, forCellReuseIdentifier: SearchFactsTableViewCell.identifier)
-        return tableView
+    let lbl: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "test"
+        return lbl
     }()
     
-    private let searchController = UISearchController(searchResultsController: nil)
-    
-    var presenter: SearchFactsPresenterProtocol?
-    var animeFacts: SearchFactsEntity? {
-        willSet {
-            animeName = searchController.searchBar.text!.lowercased()
-        }
-    }
-    public var animeName: String?
-
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUp()
-        print("view did load")
+        setup()
     }
     
-    private func setUp() {
-        print("setup view")
-//        view.backgroundColor = .secondarySystemBackground
-        title = "Search"
-        navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.prefersLargeTitles = true
+    private func setup() {
         
-        setupSearchController()
+        view.backgroundColor = .red
+        view.addSubview(lbl)
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        view.addSubview(tableView)
-        
-        tableView.snp.makeConstraints { make in
-            make.left.right.top.bottom.equalToSuperview()
+        lbl.snp.makeConstraints { make in
+            make.centerY.centerX.equalToSuperview()
         }
-        
-    }
-    
-    //MARK: - SetupSearchController
-    private func setupSearchController() {
-        print("setup search controller")
-        
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = true
-        
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search facts by anime name"
-        searchController.searchResultsUpdater = self
-        definesPresentationContext = true
-        
-    }
-    
-}
-
-extension SearchFactsViewController: SearchFactsViewProtocol {
-    func showAnimeFacts(animeFacts: SearchFactsEntity?) {
-        guard let animeFacts = animeFacts else { return }
-        self.animeFacts = animeFacts
-        self.tableView.reloadData()
-    }
-    
-}
-
-extension SearchFactsViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchFactsTableViewCell.identifier) as? SearchFactsTableViewCell, let animeFacts = animeFacts else {
-            return UITableViewCell()
-        }
-        cell.accessoryType = .disclosureIndicator
-        cell.nameLabel.text = "\(animeName?.capitalized ?? "Anime") - Fact №\(animeFacts.data[indexPath.row].fact_id)"
-        return cell
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        animeFacts?.total_facts ?? 0
-    }
-    
-    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        presenter?.didTapAnimeFactCell(with: indexPath, animeName: animeName!)
-//        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-}
-
-extension SearchFactsViewController: UISearchResultsUpdating {
-    
-    public func updateSearchResults(for searchController: UISearchController) {
-        let searchString = searchController.searchBar.text!
-            .components(separatedBy: " ")
-            .filter { !$0.isEmpty }
-            .joined(separator: "_")
-            .lowercased()
-        
-        presenter?.startSearchAnimeFacts(for: searchString)
     }
     
 }
