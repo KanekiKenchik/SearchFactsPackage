abdfa02d5a2ea764cc5623c7a277ee4797395d6f
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-28 18:44:10 +0300

    test

--- Sources/SwiftExamplePackage/СoreData/Anime+CoreDataClass.swift
+++ Sources/SwiftExamplePackage/СoreData/Anime+CoreDataClass.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-@objc(Anime)
+
 public class Anime: NSManagedObject {
 
 }
--- Sources/SwiftExamplePackage/СoreData/Fact+CoreDataClass.swift
+++ Sources/SwiftExamplePackage/СoreData/Fact+CoreDataClass.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-@objc(Fact)
+
 public class Fact: NSManagedObject {
 
 }
