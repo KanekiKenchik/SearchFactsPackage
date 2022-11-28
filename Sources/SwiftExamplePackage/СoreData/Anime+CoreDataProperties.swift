616c0b25d1866d81962291877fd72f3f582d8410
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-28 18:34:39 +0300

    test

--- Sources/SwiftExamplePackage/СoreData/Anime+CoreDataProperties.swift
+++ Sources/SwiftExamplePackage/СoreData/Anime+CoreDataProperties.swift
@@ -2,24 +2,19 @@
 //  Anime+CoreDataProperties.swift
 //  
 //
-//  Created by Афанасьев Александр Иванович on 27.11.2022.
+//  Created by Афанасьев Александр Иванович on 28.11.2022.
 //
 //
 
 import Foundation
 import CoreData
 
-
 extension Anime {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Anime> {
         return NSFetchRequest<Anime>(entityName: "Anime")
     }
-    
-    public override var description: String {
-        return "Anime"
-    }
-    
+
     @NSManaged public var name: String?
     @NSManaged public var animeFacts: Set<Fact>
 
--- Sources/SwiftExamplePackage/СoreData/Fact+CoreDataProperties.swift
+++ Sources/SwiftExamplePackage/СoreData/Fact+CoreDataProperties.swift
@@ -2,22 +2,17 @@
 //  Fact+CoreDataProperties.swift
 //  
 //
-//  Created by Афанасьев Александр Иванович on 27.11.2022.
+//  Created by Афанасьев Александр Иванович on 28.11.2022.
 //
 //
 
 import Foundation
 import CoreData
 
-
 extension Fact {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Fact> {
         return NSFetchRequest<Fact>(entityName: "Fact")
-    }
-    
-    public override var description: String {
-        return "Fact"
     }
 
     @NSManaged public var fact: String?
