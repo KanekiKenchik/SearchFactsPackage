dee7dc4ee800ad6650043646d02de3513a0ce8f6
VozmiteNaRabotu <afanasha99@mail.ru>
2022-11-28 17:54:01 +0300

    test

---
+++ Sources/SwiftExamplePackage/СoreData/File.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Афанасьев Александр Иванович on 28.11.2022.
+//
+
+import Foundation
+
+extension Anime {
+    public override var description: String {
+        return "Anime"
+    }
+}
+
+extension Fact {
+    public override var description: String {
+        return "Fact"
+    }
+}
